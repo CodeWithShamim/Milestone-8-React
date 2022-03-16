22340d813f9223e62ef1121085d3fa44161e185e
codewithshamim <shamimislamonline@gmail.com>
2022-03-16 21:29:40 +0600

    create component

--- Module 45 ( React core concepts, JSX, props, state)/my-app/src/App.js
+++ Module 45 ( React core concepts, JSX, props, state)/my-app/src/App.js
@@ -5,8 +5,22 @@ function App() {
     return ( 
         <div className="App">
            <h3>shamim</h3>
+           <New name="Shamim" email="shamimislamonline@gmail.com"></New>
+           <New name="Salman" email="salmanislamonline@gmail.com"></New>
+           <New name="Rafiq" email="rafiqislamonline@gmail.com"></New>
+           
         </div>
     );
+}
+
+function New (props) {
+    return (
+        <div>
+            <h1>Name: {props.name}</h1>
+            <h3>Email: {props.email} </h3>
+            <h5>Id: 400000</h5>
+        </div>
+    )
 }
 
 export default App;
