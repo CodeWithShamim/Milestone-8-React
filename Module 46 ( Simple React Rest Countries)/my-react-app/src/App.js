f04747831dd97baf7711de2aa98878f6cb07117d
codewithshamim <shamimislamonline@gmail.com>
2022-03-17 22:54:39 +0600

    Load data

--- Module 46 ( Simple React Rest Countries)/my-react-app/src/App.js
+++ Module 46 ( Simple React Rest Countries)/my-react-app/src/App.js
@@ -1,12 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <p>Hello</p>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+
+  // useEffect 
+  useEffect(() =>{
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      },
+  []);
+  console.log(countries);
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+
 }
 
 export default App;
