227671b76e759c88cf97d520056b2499d7e49697
codewithshamim <shamimislamonline@gmail.com>
2022-03-18 18:44:50 +0600

    Product component added

--- Conceptual session (React core concept)/my-app/src/App.js
+++ Conceptual session (React core concept)/my-app/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menubar from './components/Menubar/Menubar';
+import Product from './components/Product/Product';
 
 
 function App() {
   return (
     <div className="App">
-       <div className="bg-primary py-3">
-           <Menubar></Menubar>
-       </div>
-      <div className='container bg-warning'>
-          <h1>Hello</h1>    
+      <div className="bg-primary py-3">
+        <Menubar></Menubar>
       </div>
+
+      {/* all product  */}
+      <Product></Product>
+
     </div>
   );
 }
---
+++ Conceptual session (React core concept)/my-app/src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product=() => {
+    return (
+        <div className='container mt-4 text-primary text-uppercase'>
+            <h1>All Product</h1>
+
+        </div>
+    );
+};
+
+export default Product;
