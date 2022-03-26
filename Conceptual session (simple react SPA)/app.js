c67e5a1280226c881445ab9df964bb931733dfb6
codewithshamim <shamimislamonline@gmail.com>
2022-03-26 23:12:51 +0600

    file added

---
+++ Conceptual session (simple react SPA)/app.js
@@ -0,0 +1,11 @@
+const obj = {
+    name: 'shamim',
+    roll: 100,
+    shift: '1st'
+
+}
+console.log(obj)
+const id = '3hk45354k3535jv34c5g'
+obj[id] = 1;
+obj['namee'] = 'rock'
+console.log(obj)
