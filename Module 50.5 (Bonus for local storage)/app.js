4129bf0cb7ec8690a6b0a0b06f6498a5bd22fc67
codewithshamim <shamimislamonline@gmail.com>
2022-03-27 17:54:48 +0600

    done

--- Module 50.5 (Bonus for local storage)/app.js
+++ Module 50.5 (Bonus for local storage)/app.js
@@ -5,7 +5,10 @@ const addToDb = item => {
     } else {
         db[item] = 1;
     }
+    localStorage.setItem('cart', JSON.stringify(db));
     console.log(db)
 }
-addToDb('100')
-addToDb('100')
+addToDb('100');
+addToDb('100');
+addToDb('100');
+addToDb('101');
